b10ddd9ab784d8e7e60de1a4d2f15b0b5246004b
Munyaradzi Makosa <munya@munyamakosa.com>
2024-08-15 11:45:44 +0200

    fix : update port

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/cgi-bin/vol/transaction', (req, res) => {
     res.json(JSON.parse(response));
 });
 // Start the server
-const PORT = 3000;
+const PORT = 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
