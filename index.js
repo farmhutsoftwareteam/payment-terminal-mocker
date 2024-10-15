735ce0abac2972f9573e526f6a7efb728fc2135f
Munyaradzi Makosa <munya@munyamakosa.com>
2024-10-15 10:57:42 +0200

    feat: add refunds

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.post('/cgi-bin/vol/transaction', (req, res) => {
   } else if (req.body.header.transaction === 'sale') {
     responseFile = 'paymentResponse.json';
     setTimeout(() => sendResponse(res, responseFile, req.body.data.amount), 3000);
+  } else if (req.body.header.transaction === 'refund') {
+    responseFile = 'refundResponse.json';
+    setTimeout(() => sendResponse(res, responseFile, req.body.data.amount), 3000);
   } else {
     return res.status(400).json({ error: 'Unsupported transaction type' });
   }
@@ -24,9 +27,11 @@ function sendResponse(res, responseFile, amount) {
   let response = fs.readFileSync(path.join(__dirname, 'data', responseFile), 'utf-8');
   response = JSON.parse(response);
 
-  if (responseFile === 'paymentResponse.json') {
+  if (responseFile === 'paymentResponse.json' || responseFile === 'refundResponse.json') {
     response.data.transactionId = Math.floor(Math.random() * 10000000000).toString();
     response.data.amount = amount;
+    response.data.transactionDate = new Date().toISOString().slice(2, 10).replace(/-/g, '');
+    response.data.transactionTime = new Date().toTimeString().slice(0, 6);
   }
 
   res.json(response);
